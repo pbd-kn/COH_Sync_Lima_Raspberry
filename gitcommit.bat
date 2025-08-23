32faab67ca1bb41bf54679b1737909f9c407643c
pbd-kn <pbd@gmx.de>
2025-08-23 16:24:25 +0200

    Update direkt von pspad von 23.08.2025 16:24:03

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,11 +1,42 @@
+xxxxxxxxxxxxxxxxxxx
 @echo off
-:: Wechselt automatisch in den Ordner, in dem die Batch liegt
-cd /d "%~dp0"
+setlocal enabledelayedexpansion
 
+:: erster Parameter = Ordner der Datei, die PSPad gerade übergibt
+set startDir=%~1
+
+if "%startDir%"=="" (
+    echo Fehler: Kein Verzeichnis übergeben.
+    pause
+    exit /b 1
+)
+
+:: Suche nach .git Ordner (geht von Dateiordner nach oben)
+set searchDir=%startDir%
+:searchloop
+if exist "!searchDir!\.git" (
+    cd /d "!searchDir!"
+    goto found
+)
+cd ..
+set searchDir=%cd%
+if "%searchDir%"=="%SystemDrive%\" goto notfound
+goto searchloop
+
+:found
+echo Repository gefunden: %cd%
 set /p msg=Commit-Text eingeben: 
+if "%msg%"=="" set msg=Update von %date% %time%
 
 git add .
 git commit -m "%msg%"
-git push origin master
+git push
+goto end
 
+:notfound
+echo Kein Git-Repository gefunden!
+pause
+exit /b 1
+
+:end
 pause
